c80a5b4c8d40ecb346cf2b6dacaca85780af7af4
yisak5 <issac1162@gmail.com>
2024-05-31 22:42:23 +0900

    기본 세팅

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express')
+let cors = require('cors')
 const app = express()
-const port = 3000
+const port = 8080
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/dog', (req, res) => {
+  res.send('강아지')
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
